9a36ff611824c22ca0663e73aca8f28dd14aeedd
Hxinpeng <44037369@qq.com>
2023-03-07 08:53:42 +0800

    ?>?

--- src/main/java/com/t331/git/Main.java
+++ src/main/java/com/t331/git/Main.java
@@ -6,8 +6,8 @@ public class Main {
         System.out.println("第二次提交");
         System.out.println("第二分支的提交");
         System.out.println("正常合并");
-        System.out.println("第三次提交");
         System.out.println("冲突合并");
+        System.out.println("???");
         System.out.println("????");
     }
 }
